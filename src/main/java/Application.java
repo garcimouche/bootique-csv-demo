7a34b2d412dbccd9fd8ea9f7975fe449cd41197f
Franck Garcia <garcia.franck@gmail.com>
2018-02-16 14:17:32 -0500

    Adding multiple listener

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,18 @@
+import static java.lang.System.out;
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
 
 import io.bootique.Bootique;
 import io.bootique.csv.CSVModule;
+
 /**
- * Hello world!
- *
+ * Demo appliction for bootique-csv module
+ * @author franck
  */
 public class Application implements Module {
 
@@ -17,7 +22,10 @@ public class Application implements Module {
 
 	@Override
 	public void configure(Binder binder) {
-		CSVModule.extend(binder).addSimpleListener(s -> System.out.println(asList(s)));
+		CSVModule.extend(binder)
+		    .addRowListener(s -> out.println("l1 -> "+asList(s)))
+		    .addRowListener(s -> out.println("l2 -> "+asList(s)))
+		    .addDocumentListener(s -> out.println("ALL rows\n" + s.stream().map(Arrays::asList).collect(toList())));
 	}
 	
 }
